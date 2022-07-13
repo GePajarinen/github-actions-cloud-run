42c305286cee409dc7339109645b91ddb01ef011
Gesiane Pajarinen <gesianef@hotmail.com>
2022-07-13 14:25:30 -0300

    Update main.py

--- run/main.py
+++ run/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World v3.0 && from CICD GKE'
+    return 'Welcome to Github actions from Cloud Run v.1'
 
 
 if __name__ == '__main__':
